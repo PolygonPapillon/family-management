3be7ec89d5bc68d2a25f1b540544dedfa639f26b
Heather <heegraphicgirl@gmail.com>
2019-12-25 13:36:01 -0700

    a couple basic databases

---
+++ js/recipes.js
@@ -0,0 +1,134 @@
+{
+  recipes: {
+    meal01: {
+      name: "Bacon and Eggs";
+      ingredents: ["bacon", "eggs", "potatoes", "butter and/or bacon grease"];
+    }
+    meal02: {
+      name: "PB&J";
+      ingredents: ["peanut butter", "jelly or jam", "bread"];
+    }
+    meal03: {
+      name: "Stewed Tomato Soup";
+      ingredents: ["jar of stewed tomatoes"];
+    }
+    meal04: {
+      name: "Funeral Potatoes";
+      ingredents: ["bag of mix", "maybe something else"];
+    }
+    meal05: {
+      name: "Crab Salad";
+      ingredents: [
+        "fake crab",
+        "crackers",
+        "mayo",
+        "dill",
+        "garlic powder",
+        "salt",
+        "pepper"
+      ];
+    }
+    meal06: {
+      name: "Baked Fish";
+      ingredents: [
+        "salmon fillet",
+        "dill",
+        "mayo",
+        "garlic powder",
+        "onion powder",
+        "salt",
+        "parmesan cheese"
+      ];
+    }
+    meal07: {
+      name: "Steak";
+      ingredents: [
+        "steak",
+        "garlic powder",
+        "onion powder",
+        "salt",
+        "butter",
+        "smoked paprika"
+      ];
+    }
+    meal08: {
+      name: "Baked Chicken Thigh";
+      ingredents: [
+        "chicken thigh - skin on, bone in",
+        "garlic powder",
+        "onion powder",
+        "salt",
+        "butter"
+      ];
+    }
+    meal09: {
+      name: "Rice-a-roni";
+      ingredents: ["box of mix", "butter", "sometimes milk"];
+    }
+    meal10: {
+      name: "Red Lobster biscuits";
+      ingredents: [
+        "box of biscuit mix",
+        "bag of topping mix",
+        "butter",
+        "milk"
+      ];
+    }
+    meal11: {
+      name: "Cafe Rio tortillas";
+      ingredents: [
+        "tortillas",
+        "refried beans",
+        "shredded cheddar",
+        "spice blend",
+        "non-stick cooking spray"
+      ];
+    }
+    meal12: {
+      name: "Japanese fried rice";
+      ingredents: [
+        "leftover rice",
+        "eggs",
+        "carrots",
+        "onion",
+        "garlic clove(s)",
+        "miso",
+        "sesame oil"
+      ];
+    }
+    meal13: {
+      name: "fruity drink";
+      ingredents: [
+        "leftover baby foods",
+        "milk, yogurt, or cream",
+        "fruirs",
+        "veggies",
+        "protein powder",
+        "peanut butter",
+        "honey",
+        "oatmeal and/or barley"
+      ];
+    }
+    meal14: {
+      name: "Vitamix cheesy potato soup";
+      ingredents: [
+        "bacon bits",
+        "shredded cheddar",
+        "potatoes",
+        "milk or cream",
+        "dried onion bits"
+      ];
+    }
+    meal15: {
+      name: "Vitamix cheesy broccoli cauliflower soup";
+      ingredents: [
+        "bacon bits",
+        "shredded cheddar",
+        "cauliflower",
+        "broccoli",
+        "milk or cream",
+        "dried onion bits"
+      ];
+    }
+  }
+}
---
+++ js/token-store.js
@@ -0,0 +1,8 @@
+{
+    item: {
+        name: 'stay up and watch KBC episode with Mom';
+        type: 'experience';
+        tokenCost: 30;
+        pic: '';
+    }
+}
